2dee9907cfe99e5dd2e3df2a8bf4c5eaae80668e
rieszbanach <riesz_banach@hotmail.com>
2020-08-20 11:18:37 -0500

    Add test_classifier

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,11 +1,14 @@
-# content of test_sample.py
-
-def inc(x):
-    return x + 1
-
-
-def test_answer():
-    assert inc(3) == 4
+def test_classifier():
+	
+	try:
+		import GNLOOKUP.Classifiers.spanish_classifier as classifiers
+	except BaseException:
+		import sys
+		sys.path.append("../")
+		sys.path.append("../GNLOOKUP/Classifiers")
+		print(sys.path)
+	import GNLOOKUP.Classifiers.spanish_classifier as classifiers
+	assert classifiers.minimal_performance_metric(classifiers.articles_filenames())[1] >= 0.85
 
 
 
